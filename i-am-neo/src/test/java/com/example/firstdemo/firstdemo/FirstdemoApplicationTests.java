84741ad3b5795ed724137e72a1ced09448fe4945
SayyedNizar <151517789+SayyedNizar@users.noreply.github.com>
2025-09-17 11:35:03 +0530

    Added i-am-neo repo files

---
+++ i-am-neo/src/test/java/com/example/firstdemo/firstdemo/FirstdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstdemo.firstdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
